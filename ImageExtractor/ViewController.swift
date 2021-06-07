da364656ebf9e52a562d38a011309e8ee8d9980a
Vishnupriya <r.vishnupriya@tcs.com>
2021-06-07 10:29:17 +0530

    Initial Commit

---
+++ ImageExtractor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageExtractor
+//
+//  Created by Mobility on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
